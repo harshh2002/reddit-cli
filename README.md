a8b790c9f74a9adaa96273ea3933338380d93bff
Harsh Pratap Singh <harsh@Harshs-MacBook-Pro.local>
2022-07-28 23:46:32 +0530

    readme

---
+++ README.md
@@ -0,0 +1,7 @@
+Just a basic reddit CLI tool.
+
+
+# USAGE
+
+	--print, -p = print on terminal
+	no flags = open browser
--- reddit.mjs
+++ reddit.mjs
@@ -11,7 +11,7 @@ const children = data.data.children
 const randomPost = children[Math.floor(Math.random() * children.length)]
 const link = `https://www.reddit.com${randomPost.data.permalink}`
 
-if (argv.print){
+if (argv.print || argv.p) {
     console.log(`
         title: ${randomPost.data.title}
         link: ${link}
