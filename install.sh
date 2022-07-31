5f193e0775d4785b20583e101ed620367b272601
Harsh Pratap Singh <harshh2002@gmail.com>
2022-07-31 19:08:44 +0530

    update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 	git clone https://github.com/harshh2002/reddit-cli.git
 	cd ./reddit-cli
-  npm i
-  sudo npm i --location=global
-  cd ..
-  rm -rf reddit-cli
+  	npm i
+  	sudo npm i --location=global
+  	cd ..
+ 	rm -rf reddit-cli
